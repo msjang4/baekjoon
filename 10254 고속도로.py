6340fd106615f8f853858483ed6bb0fb794b751f
msjang4 <msjang7115@gmail.com>
2022-12-04 22:52:06 +0900

    Update 10254 고속도로.py

--- 10254 고속도로.py
+++ 10254 고속도로.py
@@ -32,7 +32,7 @@ for _ in range(t):
     arr.sort() # x좌표가 작은 순으로 정렬해야 dy/dx 순으로 정렬시 반시계방향으로 정렬됨
     std = arr[0] #기준점
 
-    arr=sorted(arr[1:],key=lambda x: (slope(std,x),-x[1])) # 평행한 경우에는 y가 큰 것부터 scan해야 양끝점이 됨. (ccw는 평행하더라도 거리가 멀면 ccw값이 더큼)
+    arr=sorted(arr[1:],key=lambda x: slope(std,x)) # 평행한 경우에는 x가 작은 것부터 scan해야 양끝점이 되나 stable 정렬이므로 명시하지 않아도됨.
     # arr = sorted(arr[1:], key = cmp_to_key(lambda p1,p2: -cccw(std,p1,p1,p2) ))
     # ccw로 정렬하는 방법이있는데 같은 시간복잡도여도 단순 기울기 계산보다 연산이 많이 필요해서 시간초과 난다.
 
@@ -44,7 +44,7 @@ for _ in range(t):
             fir = stk[-1] # fir,sec,nex의 ccw값에 상관없이 어차피  fir는 다시 스택에 들어가므로
             #pop하지 않고 top으로 값만 추출
 
-            if cccw(fir,sec,sec,nex)>=0: #변에 점이 여러 개 있는 (= 평행한) 경우에는 가장 양 끝 점만 개수에 포함
+            if cccw(fir,sec,sec,nex)>0: #변에 점이 여러 개 있는 (= 평행한) 경우에는 가장 양 끝 점만 개수에 포함
                 stk.append(sec)  #fir, sec,next가 반시계방향이라 sec를 convex hull 요소로 넣는 것.
                 break
             # fir,sec,next가 시계방향이면 while문을 한번더 돌게 되고 그때는 fir가 sec가 되고 fir이전게 fir가 됨.
