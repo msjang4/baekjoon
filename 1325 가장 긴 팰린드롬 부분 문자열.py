543eab7aec01c93a0a2d9d228acd10b1c2b7dd6e
msjang4 <msjang7115@gmail.com>
2023-01-15 15:46:37 +0900

    Create 1325 가장 긴 팰린드롬 부분 문자열.py

---
+++ 1325 가장 긴 팰린드롬 부분 문자열.py
@@ -0,0 +1,42 @@
+import sys
+
+try:
+    read = open('stdout.txt', 'r').readline
+except:
+    read = sys.stdin.readline
+
+inp=read().strip()
+
+s=''
+for i in range(len(inp)):
+    s+='#' #짝수 팰린드롬을 찾기위한 거
+    s+=inp[i]
+s+='#'
+n= len(s)
+dp = [0]*n
+ans=0
+l=-1
+r=-1
+for i in range(n):
+
+    if i>r:
+        size = 1
+    else:
+        size = min(r-i+1,dp[l+r-i])
+        #center point는 l+r/2임 i'는 cp - (i-cp)이므로 2*cp-i = l+r-i로 i'를 구할 수 있음
+        #[l,r]구간내는 팰린드롬이 보장되므로 i+size가 r+1일때부터 탐색해야하므로 r+1-i = size임
+
+    while i-size >=0 and i+size<n and s[i-size] == s[i+size]:
+        size+=1
+    size-=1
+    if i+size>r:
+        r= i+size
+        l= i-size
+    dp[i] = size
+    ans = max(dp[i] , ans)
+
+
+print(ans)
+
+
+
